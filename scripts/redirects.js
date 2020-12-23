b9920157655d1cc4ec71751b67c301b1c1101b43
wnm210 <49320100+wnm210@users.noreply.github.com>
2020-12-23 19:55:32 +0100

    Updated some things

--- scripts/redirects.js
+++ scripts/redirects.js
@@ -3,15 +3,4 @@ window.onload = chooseAv;
 function chooseAv() {
 	let ranType = Math.floor(Math.random() * avArray.length);
 	let genL = avArray[ranType];
-	if (genL == 'vave') {
-		document.getElementById('avFrame').src = `https://${avArray[ranType]}bot.pl/`;
-		document.getElementById('avImg').style.display = 'none';
-		document.getElementById('avFrame').style.display = 'block';
-	} else if (genL == 'sparfy') {
-		document.getElementById('color').style.backgroundColor = '#08192d';
-		document.getElementById("avImg").src = `assets/ads/${avArray[ranType]}.gif`;
-		document.getElementById("avImg").style.display = 'block';
-		document.getElementById("avFrame").style.display = 'none';
-		document.getElementById('avImg').src = `assets/ads/sparfy.gif`;
-	}
 }
