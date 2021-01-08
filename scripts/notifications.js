dd6fca0574c2f494e0aecb53d2f5a94213129df0
wnm210 <49320100+wnm210@users.noreply.github.com>
2021-01-08 23:21:47 +0100

    Create notifications.js

---
+++ scripts/notifications.js
@@ -0,0 +1,25 @@
+let notifications_home = document.getElementById('notifi-home');
+let notifications_dash = document.getElementById('notifi-dash');
+
+window.onload = getnotify();
+function getNotify() {
+  notifications_dash.innerHTML = '<div class="notification --red">
+  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+  <h1>We are still working on our website! Tell us about the bugs <a href="https://vavebot.pl/support">
+  <strong>here</strong>
+  </a>
+  </h1>
+  </div>';
+  
+  notifications_dash.innerHTML = '<div class="notification --green">
+  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+  <h1>Wanna help us? Boost our <a href="https://vavebot.pl/support">
+  <strong>support server</strong>
+  </a>
+  </h1>
+  </div>';
+  
+  notifications_home.innerHTML = '<div class="notification --normal">
+	<h1>To new release: <strong id="countdown">loading...</strong></h1>
+	</div>';
+}
