497cd5d8d9fcc7526c064d1933991b85ee7df371
wnm210 <49320100+wnm210@users.noreply.github.com>
2021-01-09 11:07:49 +0100

    Update notifications.js

--- scripts/notifications.js
+++ scripts/notifications.js
@@ -2,7 +2,10 @@ let notifications_home = document.getElementById('notifi-home');
 let notifications_dash = document.getElementById('notifi-dash');
 window.onload = getNotify();
 function getNotify() {
-  notifications_dash.innerHTML = '<div class="notification --red"><span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span><h1>We are still working on our website! Tell us about the bugs <a href="https://vavebot.pl/support"><strong>here</strong></a></h1></div><div class="notification --green"><span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span><h1>Wanna help us? Boost our <a href="https://vavebot.pl/support"><strong>support server</strong></a></h1></div>';
-  notifications_home.innerHTML = '<div class="notification --normal"><h1>To new release: <strong id="countdown">loading...</strong></h1></div>';
+  if (notifications_home == null) {
+   notifications_dash.innerHTML = '<div class="notification --red"><span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span><h1>We are still working on our website! Tell us about the bugs <a href="https://vavebot.pl/support"><strong>here</strong></a></h1></div><div class="notification --green"><span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span><h1>Wanna help us? Boost our <a href="https://vavebot.pl/support"><strong>support server</strong></a></h1></div>';
+  } else if (notifications_dash == null) {
+   notifications_home.innerHTML = '<div class="notification --normal"><h1>To new release: <strong id="countdown">loading...</strong></h1></div>'; 
+  }
   console.log('%c[✓] Notifications loaded successful!', 'color: #5ac0de');
 }
